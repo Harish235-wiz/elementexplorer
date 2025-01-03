2859ef70dbc0d7ab3d9016e9fd5510e390a5cae0
Harish Raghav Venkatesan <harish.jee.wiz@gmail.com>
2025-01-04 01:17:21 +0530

    Moved files out of the 'app' folder and updated imports

--- app.py
+++ app.py
@@ -134,6 +134,8 @@ element_data = {
 @app.route('/')
 def home():
     return render_template('index.html')
+if __name__ == "__main__":
+    app.run(debug=True)
 
 @app.route('/search', methods=['POST'])
 def search_element():
